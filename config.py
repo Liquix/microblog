5f86915ea60e8da849186df29454e9c24eb1fc47
Stevie Ulfelder <Liquix@Lavabit.com>
2013-03-25 23:19:29 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# flask-wtf
+CSRF_ENABLED = True
+SECRET_KEY = 'zzghfxj6hn'
+
+# openid
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }
+]
+
+# database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
---
+++ readme.md
@@ -0,0 +1,15 @@
+# microblog
+
+small microblog project written in python & html using the flask framework.
+
+## flask extensions
+
+* flask-login
+* flask-openid
+* flask-mail
+* flask-sqlalchemy
+	* sqlalchemy-migrate
+* flask-whooshalchemy
+* flask-wtf
+* flask-babel
+* flup
